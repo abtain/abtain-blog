7906e891a09ed8d8af65272f5f96de3f8a590a6e
Dan Quellhorst <dan@abtain.com>
2010-11-23 15:44:23 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbtainBlog::Application.config.secret_token = '0d15d6c1c805b5e51e8627c50d41371436d4c026b23d1f3f049c4e77b0b45921c0a2bbfa317fc4062def7f5e5d80e0596a7baab6e9652eccebafca7d1bb22c0c'
